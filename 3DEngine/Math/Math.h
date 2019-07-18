11b5829cb7886cde452378d833f421ef03d49bcc
dingdekai <dekai.ding@shen021.com>
2019-07-18 14:54:11 +0800

    [Add] math common part.

---
+++ 3DEngine/Math/Math.h
@@ -0,0 +1,12 @@
+#ifndef __MATH_MATH_H__
+#define __MATH_MATH_H__
+
+#include "../Common/Macro.h"
+
+ENGINE_NAMESPACE_START
+
+
+
+ENGINE_NAMESPACE_END
+
+#endif
---
+++ 3DEngine/Math/MathCommon.h
@@ -0,0 +1,16 @@
+#ifndef __MATH_MATHCOMMON_H__
+#define __MATH_MATHCOMMON_H__
+
+#include "../Common/Macro.h"
+
+ENGINE_NAMESPACE_START
+
+const float PI = 3.1415926535f;
+const float PI2 = Pi * 2.0f;
+const float PiOVER2 = Pi / 2.0f;
+
+
+
+ENGINE_NAMESPACE_END
+
+#endif
