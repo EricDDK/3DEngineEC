7f9ffc7b9007ccf82d2a6fabd3f07232021126d9
dingdekai <dekai.ding@shen021.com>
2019-07-19 11:22:09 +0800

    [Add] matrix3 unit test and util.

---
+++ 3DEngine/Math/MatrixUtil.h
@@ -0,0 +1,18 @@
+#ifndef __MATH_MATRIX3_UTIL_H__
+#define __MATH_MATRIX3_UTIL_H__
+
+#include "Matrix3.h"
+
+#include "../Common/Macro.h"
+
+ENGINE_NAMESPACE_START
+
+class MatrixUtil
+{
+public:
+	
+};
+
+ENGINE_NAMESPACE_END
+
+#endif
