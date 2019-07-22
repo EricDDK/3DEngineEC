7133fb5f98a7f7bf9600902773a0c9eb68160aea
dingdekai <dekai.ding@shen021.com>
2019-07-22 16:29:07 +0800

    [Add] some libs and dll.

--- 3DEngine/Common/3DEngine.h
+++ 3DEngine/Common/3DEngine.h
@@ -1,6 +1,8 @@
 #ifndef __COMMON_3DENGINE_H__
 #define __COMMON_3DENGINE_H__
 
+#include "glad/glad.h"
+
 #include "Macro.h"
 #include "../Platform/PlatformConfig.h"
 #include "../Base/MemoryPool/MemoryTool.h"
