7b9c82f4a1459a8eb96a7424f657f13093c69ebc
dingdekai <dekai.ding@shen021.com>
2019-07-23 18:31:59 +0800

    [Fix] the link error and vs config error.

--- 3DEngine/Common/Log.h
+++ 3DEngine/Common/Log.h
@@ -9,41 +9,7 @@
 
 ENGINE_NAMESPACE_START
 
-void log(const char * format, ...)
-{
-	int bufferSize = MAX_LOG_LENGTH;
-	char* buf = nullptr;
-	int nret = 0;
-	va_list args;
-	do
-	{
-		buf = new (std::nothrow) char[bufferSize];
-		if (buf == nullptr)
-			return;
-		va_start(args, format);
-		nret = vsnprintf(buf, bufferSize - 3, format, args);
-		va_end(args);
-		if (nret >= 0)
-		{
-			if (nret <= bufferSize - 3)
-			{
-				break;
-			}
-			else
-			{
-				bufferSize = nret + 3;
-				delete[] buf;
-			}
-		}
-		else
-		{
-			bufferSize *= 2;
-			delete[] buf;
-		}
-	} while (true);
-	buf[nret] = '\0';
-	std::cout << buf << std::endl;
-}
+void log(const char * format, ...);
 
 ENGINE_NAMESPACE_END
 
