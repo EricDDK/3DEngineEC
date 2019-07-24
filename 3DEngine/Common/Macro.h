67001b9a4f5b77933cfa259352eafd94cd5929a0
dingdekai <dekai.ding@shen021.com>
2019-07-24 11:34:55 +0800

    [Add] assert macro and lib export func macro.

--- 3DEngine/Common/Macro.h
+++ 3DEngine/Common/Macro.h
@@ -1,6 +1,8 @@
 #ifndef _COMMON_MACRO_H__
 #define _COMMON_MACRO_H__
 
+#include <assert.h>
+
 #ifdef __cplusplus
 	#ifndef ENGINE_NAMESPACE_START
 	#define ENGINE_NAMESPACE_START namespace engine {
@@ -18,5 +20,20 @@
 	#define ENGINE_NAMESPACE_END
 	#define ENGINE_NAMESPACE_USE
 #endif 
+
+#ifdef _WIN32
+	#ifndef EC_DLL
+	#define EC_DLL __declspec(dllexport)
+	#endif // !ET_DLL
+#else
+	#ifndef ET_DLL
+	#define ET_DLL __attribute__((visibility("default")))
+	#endif // !ET_DLL
+#endif
+
+#ifndef EC_ASSERT
+#define EC_ASSERT assert
+#endif // !ET_ASSERT
+
 
 #endif // !_COMMON_MACRO_H__
