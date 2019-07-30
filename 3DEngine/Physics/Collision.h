bc02c3c99dbc63e0bf51c06a108ad0d087982a44
dingdekai <dekai.ding@shen021.com>
2019-07-30 10:26:17 +0800

    [Add] empty class about collision.

---
+++ 3DEngine/Physics/Collision.h
@@ -0,0 +1,21 @@
+#ifndef _PHYSICS_COLLISION_H__
+#define _PHYSICS_COLLISION_H__
+
+#include "../Common/Macro.h"
+#include "../Math/Math.h"
+#include "LineSegment.h"
+
+ENGINE_NAMESPACE_START
+
+// static class member function to check collision between objects with different type.
+class Collision
+{
+public:
+
+
+
+};
+
+ENGINE_NAMESPACE_END
+
+#endif
